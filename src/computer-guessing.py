3c277417245d01e44f8126892268008c50aebf0e
Jerohan1 <95771724+Jerohan1@users.noreply.github.com>
2024-09-13 12:02:09 +0200

    strategies completed

--- src/computer-guessing.py
+++ src/computer-guessing.py
@@ -20,10 +20,39 @@ def input_selection(prompt: str, options: list[str]) -> str:
 print("Please thing of a number from 1 to 20, both included.")
 print("Let me know how good my guess is.\n")
 
-# Here, we implement the computer's strategy for guessing
+# Here, we implement the computer's strategy for guessing (strategy 1.)
 # the number you are thinking of. Don't lie to the
 # computer. It won't punish you, but it will frown upon it.
-for guess in range(1, 21):
+# for guess in range(1, 21):
+#     result = input_selection(
+#         "I'm guessing {}\nHow is my guess?".format(guess),
+#         ["low", "hit", "high"]
+#     )
+#     if result == "hit":
+#         print("Wuhuu!")
+#         break
+
+#     print("I must have been too low, right?", result)
+
+# # guessing strategy 2.
+
+# for guess in range(1, 21, -1):
+#     result = input_selection(
+#         "I'm guessing {}\nHow is my guess?".format(guess),
+#         ["low", "hit", "high"]
+#     )
+#     if result == "hit":
+#         print("Wuhuu!")
+#         break
+
+#     print("I must have been too high, right?", result)
+
+# guessing strategy 3.
+
+upper, lower = 20, 1
+guess = upper // 2
+result = ''
+while result != 'hit':
     result = input_selection(
         "I'm guessing {}\nHow is my guess?".format(guess),
         ["low", "hit", "high"]
@@ -31,5 +60,9 @@ for guess in range(1, 21):
     if result == "hit":
         print("Wuhuu!")
         break
-
-    print("I must have been too low, right?", result)
+    elif result == 'low':
+        lower = guess + 1
+    else: 
+        upper = guess - 1
+    
+    guess = (upper + lower) // 2
